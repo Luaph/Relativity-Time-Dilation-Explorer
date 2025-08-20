8111005e41522765ffe8bf216416f4d6570e9994
Laph <123279896+Luaph@users.noreply.github.com>
2025-08-20 11:23:08 +0200

    - removed .o files
    MY BAD

--- src/constants.c
+++ src/constants.c
@@ -1,4 +1,4 @@
 #include "constants.h"
 
-const double c = 299792458.0;     // exact by definition
-const double G = 6.67430e-11;     // CODATA 2018
+const double c = 299792458.0;
+const double G = 6.67430e-11;
